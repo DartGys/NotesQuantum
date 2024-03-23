815b00cac4a58641009e937634c69a74bf3742b0
DartGys <boda20043@gmail.com>
2024-03-23 16:52:24 +0200

    added note service

---
+++ Notes.BLL/Interfaces/ICrud.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Notes.BLL.Interfaces
+{
+    public interface ICrud<TModel> where TModel : class
+    {
+        Task<IEnumerable<TModel>> GetAllAsync();
+
+        Task<TModel> GetByIdAsync(int id);
+
+        Task AddAsync(TModel model);
+
+        Task UpdateAsync(TModel model);
+
+        Task DeleteAsync(int modelId);
+    }
+}
---
+++ Notes.BLL/Services/NoteService.cs
@@ -0,0 +1,41 @@
+﻿using Notes.BLL.Interfaces;
+using Notes.BLL.Models;
+using Notes.DL.Data;
+
+namespace Notes.BLL.Services
+{
+    public class NoteService : INoteService
+    {
+        private readonly NotesDbContext _context;
+
+        public NoteService(NotesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(NoteModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int modelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<NoteModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteModel> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(NoteModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
