aada8cd4dc6ec97403679a61a47b963211c1aabc
DartGys <boda20043@gmail.com>
2024-03-23 15:34:01 +0200

    Init migration added

--- Notes.WebAPI/Program.cs
+++ Notes.WebAPI/Program.cs
@@ -3,12 +3,11 @@ using Notes.DL;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddDataServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDataServices(builder.Configuration);
 
 var app = builder.Build();
 
