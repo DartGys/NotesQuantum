afc06490269be9daf756d2edcf998b81136b2c35
DartGys <boda20043@gmail.com>
2024-03-23 15:22:25 +0200

    Note configuration implemented

---
+++ Notes.DL/Data/Configurations/NoteConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.DL.Data.Entities;
+
+namespace Notes.DL.Data.Configurations
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Text) 
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            builder.Property(x => x.CreateDate)
+                .HasColumnType("timestamp")
+                .IsRequired();
+        }
+    }
+}
